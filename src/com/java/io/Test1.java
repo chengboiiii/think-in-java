7c74d00b60c50668b05d3e6e2fa1a10b077848c8
Administrator <Administrator@guangnian-PC>
2017-07-28 11:46:19 +0800

    1

--- src/com/java/io/Test1.java
+++ src/com/java/io/Test1.java
@@ -1,5 +1,13 @@
 package com.java.io;
 
 public class Test1 {
-
+	public static void main(String[] args) {
+		
+		
+		String str = "17:30:00-18:30:00";
+		String str1=str.substring(0, 5);
+		String str2=str.substring(8, 14);
+		
+		System.out.println(str1);
+	}
 }
